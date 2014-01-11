a3a842effc2741c4df81423088bfa4a1afa5a2fb
Ali Mabsoute <alimabsoute@Alis-MacBook-Air.local>
2014-01-11 04:54:39 -0500

    Initial Commit

---
+++ JSONDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  JSONDemo
+//
+//  Created by Ali Mabsoute on 1/11/14.
+//  Copyright (c) 2014 Ali Mabsoute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ JSONDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JSONDemo
+//
+//  Created by Ali Mabsoute on 1/11/14.
+//  Copyright (c) 2014 Ali Mabsoute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
